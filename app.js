5ffd8ba8741ec52bdee46b1091f987a101a00cb4
pakut2 <kutny.pawel@gmail.com>
2021-01-07 23:05:05 +0100

    commit

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("service-worker.js");
+}
+
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
